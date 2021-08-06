35f00775e98dc6802010e5b96bd875f1b6d1c694
KIMSUNGYEUN <201605ksy@dsm.hs.kr>
2021-08-06 22:47:17 +0900

    Update BirthdayParty.cs

--- party_planer/BirthdayParty.cs
+++ party_planer/BirthdayParty.cs
@@ -7,18 +7,12 @@ using System.Windows.Forms;
 
 namespace party_quote
 {
-    class BirthdayParty
+    class BirthdayParty : Party
     {
-        public const int CostOfFoodPerPerson = 25;
-
-        public decimal CostOfDecorations = 0;
-        public bool fancyDecorations;
         public int CakeSize;
 
-        public BirthdayParty(int numberOfPeople, bool fancyDecorations, string cakeWriting)
+        public BirthdayParty(int numberOfPeople, bool fancyDecorations, string cakeWriting) : base(numberOfPeople, fancyDecorations)
         {
-            this.numberOfPeople = numberOfPeople;
-            this.fancyDecorations = fancyDecorations;
             CalculateCakeSize();
             this.CakeWriting = cakeWriting;
             CalculateCostOfDecorations(fancyDecorations);
@@ -55,37 +49,25 @@ namespace party_quote
             }
         }
 
-        public decimal CalculateCost()
+        public override decimal CalculateCost()
         {
-            decimal TotalCost = CostOfDecorations + (CostOfFoodPerPerson * NumberOfPeople);
             decimal CakeCost;
             if (CakeSize == 8)
                 CakeCost = 40M + CakeWriting.Length * .25M;
             else
                 CakeCost = 75M + CakeWriting.Length * .25M;
-            return TotalCost + CakeCost;
+            return base.CalculateCost() + CakeCost;
         }
 
-        private int numberOfPeople;
-        public int NumberOfPeople
+        public override int NumberOfPeople
         {
-            get { return numberOfPeople; }
+            get { return base.NumberOfPeople; }
             set
             {
-                numberOfPeople = value;
-                CalculateCostOfDecorations(fancyDecorations);
+                base.NumberOfPeople = value;
                 CalculateCakeSize();
                 this.CakeWriting = cakeWriting;
             }
-        }
-
-        public void CalculateCostOfDecorations(bool fancy)
-        {
-            fancyDecorations = fancy;
-            if (fancy)
-                CostOfDecorations = (NumberOfPeople * 15.00M) + 50M;
-            else
-                CostOfDecorations = (NumberOfPeople * 7.50M) + 30M;
         }
     }
 }
