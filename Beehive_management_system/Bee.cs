ff1611023ff5408ee062878951bddf92b95e62ee
KIMSUNGYEUN <201605ksy@dsm.hs.kr>
2021-08-07 23:10:41 +0900

    Create Bee.cs

---
+++ Beehive_management_system/Bee.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beehive_management_system
+{
+    class Bee
+    {
+        public Bee()
+    }
+}
