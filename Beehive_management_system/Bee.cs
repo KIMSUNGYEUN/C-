287bd3ce761695cf205f20ce85a1fd9b268a11b9
KIMSUNGYEUN <201605ksy@dsm.hs.kr>
2021-08-09 23:27:44 +0900

    Update Bee.cs

--- Beehive_management_system/Bee.cs
+++ Beehive_management_system/Bee.cs
@@ -8,6 +8,27 @@ namespace Beehive_management_system
 {
     class Bee
     {
-        public Bee()
+        public Bee(double weight)
+        {
+            this.weight = weight;
+        }
+        public virtual int ShiftsLeft
+        {
+            get { return 0; }
+        }
+
+        private double weight;
+
+        public virtual double GetHoneyConsumption()
+        {
+            double consumption;
+            if (ShiftsLeft == 0)
+                consumption = 7.5;
+            else
+                consumption = 9 + ShiftsLeft;
+            if (weight > 150)
+                consumption *= 1.35;
+            return consumption;
+        }
     }
 }
