d855d1b47956690f2461a640b8c9791d4a2cde57
KIMSUNGYEUN <201605ksy@dsm.hs.kr>
2021-09-04 21:57:01 +0900

    Update Room.cs

--- Explore the house/Room.cs
+++ Explore the house/Room.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace Explore_the_house
 {
+    interface IHidingPlace
+    {
+        string HidingPlaceName { get; }
+    }
     interface IHasExteriorDoor
     {
         string DoorDescription
@@ -31,7 +35,27 @@ namespace Explore_the_house
         }
     }
 
-    class RoomWithDoor : Room, IHasExteriorDoor
+    class RoomWithHidingPlace : Room, IHidingPlace
+    {
+        public RoomWithHidingPlace(string name, string decoration, string hidingPlaceName) : base(name, decoration)
+        {
+            this.hidingPlaceName = hidingPlaceName;
+        }
+
+        private string hidingPlaceName;
+        public string HidingPlaceName
+        {
+            get { return hidingPlaceName; }
+        }
+        public override string Description
+        {
+            get { return base.Description + " Someone could hide " + hidingPlaceName + ".";
+
+            }
+        }
+    }
+
+    class RoomWithDoor : RoomWithHidingPlace, IHasExteriorDoor
     {
         public RoomWithDoor(string name, string decoration, string doorDescription) : base(name, decoration)
         {
