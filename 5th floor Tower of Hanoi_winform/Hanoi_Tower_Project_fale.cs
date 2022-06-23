f8083bc69f9ceae2473eb9f9289cf0bd13036a34
kimsy030 <201605ksy@dsm.hs.kr>
2022-06-23 22:17:44 +0900

    Update Hanoi_Tower_Project_fale.cs

--- 5th floor Tower of Hanoi_winform/Hanoi_Tower_Project_fale.cs
+++ 5th floor Tower of Hanoi_winform/Hanoi_Tower_Project_fale.cs
@@ -5,9 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Threading;
+
 namespace Hanoi_Tower_Project
 {
     public partial class Form1 : Form
